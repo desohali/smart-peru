c28bf485df834cfd19244f58b14b39219cc3e459
hamer orlando albarran valderrama <hameralbarran@gmail.com>
2024-05-16 11:08:23 -0500

    listado de certificados

--- src/components/CertificacionesUpload.js
+++ src/components/CertificacionesUpload.js
@@ -1,12 +1,26 @@
-import { Alert, Button, Grid } from '@mui/material';
+import { LoadingButton } from '@mui/lab';
+import { Alert, Avatar, Button, Grid, List, ListItem, ListItemAvatar, ListItemText } from '@mui/material';
 import React from 'react'
 import { useParams } from 'react-router-dom';
 import Swal from 'sweetalert2';
+import WorkspacePremiumIcon from '@mui/icons-material/WorkspacePremium';
 const urlAPI = "https://yocreoquesipuedohacerlo.com";
 
 const CertificacionesUpload = () => {
 
     const imagenRef = React.useRef();
+    const [loading, setLoading] = React.useState(false);
+    const [listaDeCertificados, setListaDeCertificados] = React.useState([]);
+
+    React.useEffect(() => {
+        if (!loading) {
+            fetch(`${urlAPI}/listarCertificados`)
+                .then((response) => response.json()).then((listaCertificados) => {
+                    setListaDeCertificados(listaCertificados);
+                });
+        }
+    }, [loading]);
+
 
     const { clave } = useParams();
     if (!["ejpv2024"].includes(clave)) {
@@ -20,31 +34,55 @@ const CertificacionesUpload = () => {
             <Grid item xs={12} sm={12} md={3} lg={3}></Grid>
             <Grid item xs={12} sm={12} md={6} lg={6}>
                 <input ref={imagenRef} accept=".pdf" type='file' style={{ margin: "1rem 0rem" }}></input>
-                <Button onClick={async () => {
+                <LoadingButton
+                    size="small"
+                    loading={loading}
+                    loadingIndicator="Cargando…"
+                    onClick={async () => {
 
-                    const [imagen] = imagenRef?.current?.files;
-                    if (!imagen) {
+                        const [imagen] = imagenRef?.current?.files;
+                        if (!imagen) {
+                            Swal.fire(
+                                'SELECCIONE ARCHIVO PDF !',
+                                '',
+                                'error'
+                            );
+                            return false;
+                        }
+                        setLoading(true);
+                        const formData = new FormData();
+                        formData.append("imagen", imagen);
+
+                        const response = await fetch(`${urlAPI}/uploadCertification`, {
+                            method: "post",
+                            body: formData
+                        });
+                        const json = await response.json();
+                        imagenRef.current.value = '';
+                        setLoading(false);
                         Swal.fire(
-                            'SELECCIONE ARCHIVO PDF !',
+                            'CERTIFICADO CARGADO CORRECTAMENTE !',
                             '',
-                            'error'
+                            'success'
                         );
-                        return false;
-                    }
-                    const formData = new FormData();
-                    formData.append("imagen", imagen);
+                    }} fullWidth variant="contained">
+                    Cargar Archivo
+                </LoadingButton>
 
-                    const response = await fetch(`${urlAPI}/uploadCertification`, {
-                        method: "post",
-                        body: formData
-                    });
-                    const json = await response.json();
-                    Swal.fire(
-                        'CERTIFICADO CARGADO CORRECTAMENTE !',
-                        '',
-                        'success'
-                    );
-                }} fullWidth variant="contained">Cargar Archivo</Button>
+                {/* lista de certificados */}
+                <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+                    {listaDeCertificados.map((certificado) => (
+                        <ListItem>
+                            <ListItemAvatar>
+                                <Avatar>
+                                    <WorkspacePremiumIcon />
+                                </Avatar>
+                            </ListItemAvatar>
+                            <ListItemText primary={certificado?.name} />
+                        </ListItem>
+                    ))}
+
+                </List>
             </Grid>
             <Grid item xs={12} sm={12} md={3} lg={3}></Grid>
         </Grid>
