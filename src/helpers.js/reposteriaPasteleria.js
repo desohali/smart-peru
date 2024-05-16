750036379ddc6efc335217d9df86a19401e6ebc9
EDINSONPALACIOS <157189583+EDINSONPALACIOS@users.noreply.github.com>
2024-05-16 16:29:59 -0500

    Agrego certificados de pescados y mariscos reposteriaPasteleria.js

--- src/helpers.js/reposteriaPasteleria.js
+++ src/helpers.js/reposteriaPasteleria.js
@@ -4,8 +4,11 @@ export const participantes = [
     { dni: "46901654", nombres: "FLOR MARIANA RAMIREZ ROSARIO", certificado:"pescados_mariscos.pdf" },
     { dni: "42372968", nombres: "SARITA HAYDÉE REYES BENITES", certificado:"pescados_mariscos.pdf" },
     { dni: "44382616", nombres: "ELICIA RAMIREZ CUSTODIO", certificado:"pescados_mariscos.pdf" },
-    { dni: "72513092", nombres: "SANDRA MILAGROS PASTOR IZQUIERDO", certificado:"reposteria.pdf" },
+    { dni: "71338223", nombres: "NILDA JULISSA TORIBIO BRICEÑO", certificado:"pescados_mariscos.pdf" },
+    { dni: "47510951", nombres: "NEYDA  MILENY TORIBIO BRICEÑO", certificado:"pescados_mariscos.pdf" },
+    { dni: "42756199", nombres: "MARTHA BETZABETH RODRÍGUEZ DÍAZ", certificado:"pescados_mariscos.pdf" },
     { dni: "71804505", nombres: "NANCY ZULEMA ALVA TIRADO", certificado:"pescados_mariscos.pdf" },
     { dni: "72740259", nombres: "NELI ALEJANDRINA QUIROZ BADA", certificado:"pescados_mariscos.pdf" },
+    { dni: "74292387", nombres: "CÉSAR PERCY SANTOS TORRES", certificado:"pescados_mariscos.pdf" },
     { dni: "44278145", nombres: "LILIAN ESTHER CASTAÑEDA VILLAVICENCIO", certificado:"pescados_mariscos.pdf" },
 ];
