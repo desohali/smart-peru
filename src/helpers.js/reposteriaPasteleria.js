ad67d4708671eeeb600260ef39ec3f16e73fc38a
EDINSONPALACIOS <157189583+EDINSONPALACIOS@users.noreply.github.com>
2024-10-01 23:39:54 -0500

    Update reposteriaPasteleria.js

--- src/helpers.js/reposteriaPasteleria.js
+++ src/helpers.js/reposteriaPasteleria.js
@@ -1,6 +1,7 @@
 export const participantes = [
     { dni: "41714324", nombres: "Rivera Cardozo Pedro Lito", certificado:"CONSTANCIA CONEP24.pdf" },
     { dni: "45262156", nombres: "ERIK ADRIAN MONTOYA TERRONES", certificado:"CONSTANCIA CONEP24.pdf" },
+    { dni: "04824192", nombres: "ROLANDO ALBERTO MOLINA BOCANGEL", certificado:"CONSTANCIA CONEP24.pdf" },
     { dni: "08297889", nombres: "Fernando Ortiz Aguilar", certificado:"CONSTANCIA CONEP24.pdf" },
     { dni: "09817348", nombres: "JOSÉ ADOLFO HINOJOSA PÉREZ", certificado:"CONSTANCIA CONEP24.pdf" },
     { dni: "09817348", nombres: "Rullierd Sotomayor Terrones", certificado:"CONSTANCIA CONEP24.pdf" },
